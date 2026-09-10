df807ad3f6bb2a2d5db888f6e3fb5dcd721fb789
sirunchained <bbehnam3831@gmail.com>
2026-09-10 12:43:46 +0330

    a simple pretrain logger.

--- 01_gpt2/main.py
+++ 01_gpt2/main.py
@@ -164,6 +164,14 @@ def train(model: GPT_model, cfg: GPT_configs):
         end_frac=1.0,
     )
 
+    print(
+        f"We are going to train model {cfg.epochs} Epochs. "
+        f"Our batch size is {cfg.batch_size}. "
+        f"We have {len(train_loader)} train samples. "
+        f"We have {len(val_loader)} validation samples. "
+        f"The model will be trained in {len(train_loader) * cfg.epochs} steps. "
+    )
+
     # ==== SETUP EPOCHS DEVICE AND OPTIMIZER ====
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device=device)
