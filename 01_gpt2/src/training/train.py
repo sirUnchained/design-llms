c80cb789ce2bb4cbd9484603d5d30bfcda00093e
sirunchained <bbehnam3831@gmail.com>
2026-09-11 15:30:57 +0330

    .

--- 01_gpt2/src/training/train.py
+++ 01_gpt2/src/training/train.py
@@ -75,7 +75,7 @@ def train_model(
     tokens_seen, global_step = 0, -1
     total_steps = len(train_dataloader) * num_epochs
     start_epoch = 0
-    start_index = 34000
+    start_index = 0
 
     latest_ckpt_path = os.path.join(checkpoint_path, "latest.pt")
     os.makedirs("./training-process", exist_ok=True)
